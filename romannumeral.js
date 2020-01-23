e362afde31ff81da1226f35b12d8d52aa38fe3a8
Eliseo <eliseoandtrina@gmail.com>
2020-01-22 23:44:38 -0600

    finished the program

--- romannumeral.js
+++ romannumeral.js
@@ -1,27 +1,58 @@
-function convertToRoman(num) {
-    number.length = how many places we have. each place will be one of two things
-    if 3 places divide 100 into the number. 352 / 100
-    switch (x) {
-        case 1 : x = "I";
-            break;
-        case 5 : x = "V";
-            break;
-        case 10: x =  'X';
-            break;
-        case 50: x = 'L';
-            break;
-        case 100:  x = 'C';
-            break;
-        case 500: x = 'D';
-            break;
-        case 1000: x = 'M';
-            break;
-        default: console.log("Dont know what to do.");
 
+function convertToRoman(num) {
+    let parsingNumber = num;
+    let romanNumeralConversion = [];
+
+    while (parsingNumber > 0) {
+
+
+        if (parsingNumber >= 1000) {
+            parsingNumber -= 1000;
+            romanNumeralConversion.push("M")
+        } else if (parsingNumber >= 900) {
+            parsingNumber -= 900;
+            romanNumeralConversion.push("CM")
+        } else if (parsingNumber >= 500) {
+            parsingNumber -= 500;
+            romanNumeralConversion.push("D")
+        } else if (parsingNumber >= 400) {
+            parsingNumber -= 400;
+            romanNumeralConversion.push("CD")
+        } else if (parsingNumber >= 100) {
+            parsingNumber -= 100;
+            romanNumeralConversion.push("C")
+        } else if (parsingNumber >= 90) {
+            parsingNumber -= 90;
+            romanNumeralConversion.push("XC")
+        } else if (parsingNumber >= 50) {
+            parsingNumber -= 50;
+            romanNumeralConversion.push("L")
+        } else if (parsingNumber >= 40) {
+            parsingNumber -= 40;
+            romanNumeralConversion.push("XL")
+        } else if (parsingNumber >= 10) {
+            parsingNumber -= 10;
+            romanNumeralConversion.push("X")
+        } else if (parsingNumber >= 9) {
+            parsingNumber -= 9;
+            romanNumeralConversion.push("IX")
+        } else if (parsingNumber >= 5) {
+            parsingNumber -= 5;
+            romanNumeralConversion.push("V")
+        } else if (parsingNumber >= 4) {
+            parsingNumber -= 4;
+            romanNumeralConversion.push("IV")
+        } else if (parsingNumber >= 1) {
+            parsingNumber -= 1;
+            romanNumeralConversion.push("I")
+        }
     }
 
 
-    return num;
+
+
+
+    return romanNumeralConversion.join("");
 }
 
-convertToRoman(36);
+console.log(convertToRoman(36));
