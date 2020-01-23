6af3e51893b4b2543ecb3708e3bcdcf283a4daad
Eliseo <eliseoandtrina@gmail.com>
2020-01-22 21:45:01 -0600

    Initial commit

---
+++ romannumeral.js
@@ -0,0 +1,27 @@
+function convertToRoman(num) {
+    number.length = how many places we have. each place will be one of two things
+    if 3 places divide 100 into the number. 352 / 100
+    switch (x) {
+        case 1 : x = "I";
+            break;
+        case 5 : x = "V";
+            break;
+        case 10: x =  'X';
+            break;
+        case 50: x = 'L';
+            break;
+        case 100:  x = 'C';
+            break;
+        case 500: x = 'D';
+            break;
+        case 1000: x = 'M';
+            break;
+        default: console.log("Dont know what to do.");
+
+    }
+
+
+    return num;
+}
+
+convertToRoman(36);
